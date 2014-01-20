2e150fdec4045c11725da85ae413c1f7e6a15c53
Eurymachus <reflex_ion@hotmail.com>
2014-01-20 21:16:49 +0000

    Fixed render inventory block

--- WR-client/wirelessredstone/client/presentation/BlockRedstoneWirelessRenderer.java
+++ WR-client/wirelessredstone/client/presentation/BlockRedstoneWirelessRenderer.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
+import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.world.IBlockAccess;
+
+import org.lwjgl.opengl.GL11;
+
 import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 
@@ -20,8 +24,101 @@ public class BlockRedstoneWirelessRenderer implements
 		overrides.add(handler);
 	}
 
+	private static void renderBlockAsItem(Block par1Block, int metadata, RenderBlocks renderer) {
+		Tessellator tessellator = Tessellator.instance;
+		par1Block.setBlockBoundsForItemRender();
+		renderer.setRenderBoundsFromBlock(par1Block);
+		GL11.glRotatef(	90.0F,
+						0.0F,
+						1.0F,
+						0.0F);
+		GL11.glTranslatef(	-0.5F,
+							-0.5F,
+							-0.5F);
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(	0.0F,
+								-1.0F,
+								0.0F);
+		renderer.renderFaceYNeg(par1Block,
+								0.0D,
+								0.0D,
+								0.0D,
+								renderer.getBlockIconFromSideAndMetadata(	par1Block,
+																			0,
+																			metadata));
+		tessellator.draw();
+
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(	0.0F,
+								1.0F,
+								0.0F);
+		renderer.renderFaceYPos(par1Block,
+								0.0D,
+								0.0D,
+								0.0D,
+								renderer.getBlockIconFromSideAndMetadata(	par1Block,
+																			1,
+																			metadata));
+		tessellator.draw();
+
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(	0.0F,
+								0.0F,
+								-1.0F);
+		renderer.renderFaceZNeg(par1Block,
+								0.0D,
+								0.0D,
+								0.0D,
+								renderer.getBlockIconFromSideAndMetadata(	par1Block,
+																			2,
+																			metadata));
+		tessellator.draw();
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(	0.0F,
+								0.0F,
+								1.0F);
+		renderer.renderFaceZPos(par1Block,
+								0.0D,
+								0.0D,
+								0.0D,
+								renderer.getBlockIconFromSideAndMetadata(	par1Block,
+																			3,
+																			metadata));
+		tessellator.draw();
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(	-1.0F,
+								0.0F,
+								0.0F);
+		renderer.renderFaceXNeg(par1Block,
+								0.0D,
+								0.0D,
+								0.0D,
+								renderer.getBlockIconFromSideAndMetadata(	par1Block,
+																			4,
+																			metadata));
+		tessellator.draw();
+		tessellator.startDrawingQuads();
+		tessellator.setNormal(	1.0F,
+								0.0F,
+								0.0F);
+		renderer.renderFaceXPos(par1Block,
+								0.0D,
+								0.0D,
+								0.0D,
+								renderer.getBlockIconFromSideAndMetadata(	par1Block,
+																			5,
+																			metadata));
+		tessellator.draw();
+		GL11.glTranslatef(	0.5F,
+							0.5F,
+							0.5F);
+	}
+
 	@Override
 	public void renderInventoryBlock(Block block, int metadata, int modelID, RenderBlocks renderer) {
+		renderBlockAsItem(	Block.blocksList[block.blockID],
+							metadata,
+							renderer);
 	}
 
 	@Override
