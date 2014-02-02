5d307cdd0262f0c73b5d8a95626dac8d2086c2ca
Eurymachus <reflex_ion@hotmail.com>
2014-02-02 19:41:25 +0000

    Version update and added launch files

--- Addons/PowerConfig/PowerDirector-common/wirelessredstone/addon/powerdirector/core/lib/CoreLib.java
+++ Addons/PowerConfig/PowerDirector-common/wirelessredstone/addon/powerdirector/core/lib/CoreLib.java
@@ -4,7 +4,7 @@ public class CoreLib {
 
 	public static final String	MOD_ID				= "PowerDirector";
 	public static final String	MOD_NAME			= "Wireless Redstone - Power Director";
-	public static final String	MOD_VERSION			= "1.2";
+	public static final String	MOD_VERSION			= "1.3";
 	public static final String	MOD_DEPENDENCIES	= "required-after:WirelessRedstoneCore";
 	public static final String	MOD_RESOURCES		= "powerdirector";
 	public static final String	CLIENT_PROXY		= "wirelessredstone.addon.powerdirector.client.proxy.PowerDirectorClientProxy";
--- WR-common/wirelessredstone/core/lib/CoreLib.java
+++ WR-common/wirelessredstone/core/lib/CoreLib.java
@@ -4,7 +4,7 @@ public class CoreLib {
 
 	public static final String	MOD_ID				= "WirelessRedstoneCore";
 	public static final String	MOD_NAME			= "Wireless Redstone";
-	public static final String	MOD_VERSION			= "1.7.2.1";
+	public static final String	MOD_VERSION			= "1.7.2.2";
 	public static final String	MOD_CHANNEL			= "WR";
 	public static final String	MOD_RESOURCES		= "wirelessredstone";
 	public static final String	MOD_CLIENT_PROXY	= "wirelessredstone.client.proxy.WRClientProxy";
