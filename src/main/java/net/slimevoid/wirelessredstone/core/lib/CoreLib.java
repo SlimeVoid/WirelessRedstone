210de01e3758c197de06ea461e8d332f7ed1350e
Eurymachus <reflex_ion@hotmail.com>
2014-03-28 11:12:55 +0000

    Forgot to refactor proxy

--- src/main/java/net/slimevoid/wirelessredstone/core/lib/CoreLib.java
+++ src/main/java/net/slimevoid/wirelessredstone/core/lib/CoreLib.java
@@ -8,7 +8,7 @@ public class CoreLib {
     public static final String MOD_CHANNEL      = "WR";
     public static final String MOD_RESOURCES    = "wirelessredstone";
     public static final String MOD_DEPENDENCIES = "required-after:SlimevoidLib";
-    public static final String MOD_CLIENT_PROXY = "com.slimevoid.wirelessredstone.client.proxy.WRClientProxy";
-    public static final String MOD_COMMON_PROXY = "com.slimevoid.wirelessredstone.proxy.WRCommonProxy";
+    public static final String MOD_CLIENT_PROXY = "net.slimevoid.wirelessredstone.client.proxy.WRClientProxy";
+    public static final String MOD_COMMON_PROXY = "net.slimevoid.wirelessredstone.proxy.WRCommonProxy";
 
 }
