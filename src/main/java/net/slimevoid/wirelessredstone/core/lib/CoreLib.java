8df8ed4f43244a4d38dc609063c3b494610b2567
Eurymachus <reflex_ion@hotmail.com>
2014-06-06 14:35:20 +0100

    Build version update

--- src/main/java/net/slimevoid/wirelessredstone/core/lib/CoreLib.java
+++ src/main/java/net/slimevoid/wirelessredstone/core/lib/CoreLib.java
@@ -2,13 +2,14 @@ package net.slimevoid.wirelessredstone.core.lib;
 
 public class CoreLib {
 
-    public static final String MOD_ID           = "WirelessRedstoneCore";
-    public static final String MOD_NAME         = "Wireless Redstone";
-    public static final String MOD_VERSION      = "@VERSION@";
-    public static final String MOD_CHANNEL      = "WR";
-    public static final String MOD_RESOURCES    = "wirelessredstone";
-    public static final String MOD_DEPENDENCIES = "required-after:SlimevoidLib";
-    public static final String MOD_CLIENT_PROXY = "net.slimevoid.wirelessredstone.client.proxy.WRClientProxy";
-    public static final String MOD_COMMON_PROXY = "net.slimevoid.wirelessredstone.proxy.WRCommonProxy";
+    public static final String MOD_ID              = "WirelessRedstoneCore";
+    public static final String MOD_NAME            = "Wireless Redstone";
+    public static final String MOD_VERSION         = "@VERSION@";
+    public static final String MOD_CHANNEL         = "WR";
+    public static final String MOD_RESOURCES       = "wirelessredstone";
+    public static final String MOD_DEPENDENCIES    = "";
+    public static final String ACTUAL_DEPENDENCIES = "required-after:Forge@[@FORGEVERSION@,),SlimevoidLib@[@SLIMEVERSION@,)";
+    public static final String MOD_CLIENT_PROXY    = "net.slimevoid.wirelessredstone.client.proxy.WRClientProxy";
+    public static final String MOD_COMMON_PROXY    = "net.slimevoid.wirelessredstone.proxy.WRCommonProxy";
 
 }
